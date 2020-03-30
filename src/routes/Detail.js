64d26c2d5eb98d99b5199f56f2857efe949a40c9
chaesh <csh1708@gmail.com>
2020-03-31 08:39:57 +0900

    GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,29 @@
-export default () => "Detai";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_MOVIE = gql`
+  query getMovies($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_full
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) }
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+  return "Detail";
+};
